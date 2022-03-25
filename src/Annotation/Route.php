658be43832a9bbfb98718e3e87f538b2d121f3db
crastlin <crastlin@163.com>
2022-03-25 20:05:10 +0800

    update

--- src/Annotation/Route.php
+++ src/Annotation/Route.php
@@ -4,6 +4,10 @@
 namespace Crastlin\LaravelAnnotation\Annotation;
 
 
+use Illuminate\Cache\RedisLock;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Request;
 use ReflectionMethod;
 use Throwable;
 
@@ -303,6 +307,46 @@ class Route extends Node
                 if (!is_null($callback) && $callback instanceof \Closure)
                     $callback($scanPath);
             endforeach;
+        }
+    }
+
+    /**
+     * auto build route and node
+     */
+    static function autoBuildRouteWithNode()
+    {
+        $config = config('annotation');
+        $namespace = !empty($config['controller_namespace']) ? rtrim($config['controller_namespace'], '\\') : 'App\\Http\\Controllers';
+        $filePath = !empty($config['annotation_path']) ? rtrim($config['annotation_path'], '/') : 'data';
+        $routeBasePath = base_path($filePath . '/routes');
+        if (!empty($config['auto_create_case'])) {
+            $path = Request::instance()->path();
+            if (!empty($config['modules']) && !empty($path) && preg_match('#^(\w+)(/\w+)?(/\w+)?$#', $path, $matches)) {
+                if (!empty($matches[1]) && in_array(ucfirst($matches[1]), $config['modules'])) {
+                    $redis = Redis::connection();
+                    $distributedLock = new RedisLock($redis, 'distributed_lock:create_route_with_node', 60);
+                    try {
+                        if ($distributedLock->acquire()) {
+                            $moduleBasePath = !empty($config['controller_base']) ? rtrim($config['controller_base'], '/') : 'app/Http/Controllers';
+                            $moduleBasePath = base_path($moduleBasePath);
+                            \Crastlin\LaravelAnnotation\Annotation\Route::autoBuildRouteMapping(ltrim($path, "/{$matches[1]}"), $config['modules'], $moduleBasePath, $namespace, $routeBasePath, $config['default_middleware'] ?? [], !empty($config['auto_create_node']));
+                            $distributedLock->release();
+                        }
+                    } catch (Throwable $exception) {
+                        $distributedLock->release();
+                        Log::error('sync create route mapping was failed: ' . $exception->getMessage());
+                    }
+                }
+            }
+        }
+        // register route file
+        if (!empty($config['modules'])) {
+            foreach ($config['modules'] as $module) {
+                $module = ucfirst($module);
+                $annotationRoute = "{$routeBasePath}/{$module}/route.php";
+                if (is_file($annotationRoute))
+                    \Illuminate\Support\Facades\Route::namespace($namespace)->group($annotationRoute);
+            }
         }
     }
 
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,13 +2,7 @@
 
 namespace Crastlin\LaravelAnnotation;
 
-use Illuminate\Cache\RedisLock;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Redis;
-use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
-use Throwable;
 
 class ServiceProvider extends LaravelServiceProvider
 {
@@ -47,7 +41,6 @@ class ServiceProvider extends LaravelServiceProvider
     public function register()
     {
         $this->setupConfig();
-        $this->registerAutoCrateRouteMappingAndSaveNode();
     }
 
     /**
@@ -58,55 +51,9 @@ class ServiceProvider extends LaravelServiceProvider
     protected function setupConfig()
     {
         $source = realpath(__DIR__ . '/config.php');
-
-        $this->publishes([$source => config_path('annotation.php')]);
-
+        $userConfig = config_path('annotation.php');
+        $this->publishes([$source => $userConfig]);
         $this->mergeConfigFrom($source, 'annotation');
-    }
-
-    /**
-     * 扫描注解自动创建路由映射文件和保存权限节点
-     *
-     * @return void
-     */
-    protected function registerAutoCrateRouteMappingAndSaveNode()
-    {
-        // when debug mode, then auto sync create route mapping and menu node by the controllers annotation
-        // support new action annotation to create
-        // support edit annotation for update
-        $config = config('annotation');
-        $namespace = !empty($config['controller_namespace']) ? rtrim($config['controller_namespace'], '\\') : 'App\\Http\\Controllers';
-        $filePath = !empty($config['annotation_path']) ? rtrim($config['annotation_path'], '/') : 'data';
-        $routeBasePath = base_path($filePath . '/routes');
-        if (!empty($config['auto_create_case'])) {
-            $path = Request::instance()->path();
-            if (!empty($config['modules']) && !empty($path) && preg_match('#^(\w+)(/\w+)?(/\w+)?$#', $path, $matches)) {
-                if (!empty($matches[1]) && in_array(ucfirst($matches[1]), $config['modules'])) {
-                    $redis = Redis::connection();
-                    $distributedLock = new RedisLock($redis, 'distributed_lock:create_route_with_node', 60);
-                    try {
-                        if ($distributedLock->acquire()) {
-                            $moduleBasePath = !empty($config['controller_base']) ? rtrim($config['controller_base'], '/') : 'app/Http/Controllers';
-                            $moduleBasePath = base_path($moduleBasePath);
-                            \Crastlin\LaravelAnnotation\Annotation\Route::autoBuildRouteMapping(ltrim($path, "/{$matches[1]}"), $config['modules'], $moduleBasePath, $namespace, $routeBasePath, $config['default_middleware'] ?? [], !empty($config['auto_create_node']));
-                            $distributedLock->release();
-                        }
-                    } catch (Throwable $exception) {
-                        $distributedLock->release();
-                        Log::error('sync create route mapping was failed: ' . $exception->getMessage());
-                    }
-                }
-            }
-        }
-        // register route file
-        if (!empty($config['modules'])) {
-            foreach ($config['modules'] as $module) {
-                $module = ucfirst($module);
-                $annotationRoute = "{$routeBasePath}/{$module}/route.php";
-                if (is_file($annotationRoute))
-                    Route::namespace($namespace)->group($annotationRoute);
-            }
-        }
     }
 
 }
